3face7f9fe0f322c62d0bb9f0b54c411f1d17c32
Jérémy Belleau <belleauj22@gmail.com>
2024-05-12 17:41:12 -0400

    feat: making pdf from react

--- src/components/studentCenter/listView/Cells/GetResumeCell.tsx
+++ src/components/studentCenter/listView/Cells/GetResumeCell.tsx
@@ -1,6 +1,9 @@
 import { getDatabase, onValue, ref } from "firebase/database";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import { useEffect, useState } from "react";
 import { Cell } from "react-aria-components";
+import ReactDOM from "react-dom";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import DownloadIcon from "../../../../assets/Icons/DownloadIcon";
@@ -51,15 +54,59 @@ const GetResumeCell = ({id}: {id: string}) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleGetResume = () => {
-    const rs = Object.values(resumesList).find(resume => resume.id===id);
-    console.log(rs);
+  const TEST = () => {
+    return (
+      <div>REACT NODE
+         <div>
+        <h1>This content will be included in the PDF</h1>
+        <p>More content...</p>
+      </div>
+      </div>
+    );
   };
+
+  const captureAndPrint = () => {
+    // Render the content of the div off-screen
+    const divToPrint = document.createElement('div');
+    divToPrint.id = 'divToPrint';
+    divToPrint.style.position = 'absolute';
+    divToPrint.style.left = '-9999px';
+    divToPrint.style.top = '-9999px';
+    document.body.appendChild(divToPrint);
+    ReactDOM.render(<TEST/>, divToPrint);
+    if (divToPrint) {
+      html2canvas(document.getElementById('divToPrint') as HTMLElement, { scrollY: -window.scrollY })
+        .then((canvas) => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF();
+          const pdfWidth = pdf.internal.pageSize.getWidth();
+          const pdfHeight = pdf.internal.pageSize.getHeight();
+          const imgHeight = (canvas.height * pdfWidth) / canvas.width;
+          let position = 0;
+
+          pdf.addImage(imgData, 'JPEG', 0, position, pdfWidth, imgHeight);
+          position -= pdfHeight;
+
+          while (position > -canvas.height) {
+            pdf.addPage();
+            pdf.addImage(imgData, 'JPEG', 0, position, pdfWidth, imgHeight);
+            position -= pdfHeight;
+          }
+
+          pdf.save("download.pdf");
+        });
+    }
+  };
+
+  // const handleGetResume = () => {
+  //   const rs = Object.values(resumesList).find(resume => resume.id===id);
+  //   printDocument();
+  // };
 
   return (
     <ReactAriaCell>
       <Content>
-        <DownloadButton onClick={handleGetResume}>
+        <DownloadButton onClick={captureAndPrint}>
           <DownloadIcon />
         </DownloadButton>
       </Content>
