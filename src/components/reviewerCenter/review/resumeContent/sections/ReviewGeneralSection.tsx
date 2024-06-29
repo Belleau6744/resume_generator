b72535fc553ef7b06f3925b97c0790dd3eff7327
Jérémy Belleau <71091127+Belleauu@users.noreply.github.com>
2024-06-29 11:53:10 -0400

    update general information layout

--- src/components/reviewerCenter/review/resumeContent/sections/ReviewGeneralSection.tsx
+++ src/components/reviewerCenter/review/resumeContent/sections/ReviewGeneralSection.tsx
@@ -1,31 +1,74 @@
-import { Typography } from "@mui/material";
+import { InputLabel, TextField, Typography, styled } from "@mui/material";
 import { GeneralInfoType } from "@types";
 import { LangLevel, LangList } from "utils/Languages";
 
+const StyledTextField = styled(TextField)({
+    '& .MuiInputBase-input': {
+      caretColor: 'transparent', // Hides the caret
+    },
+  });
 type ReviewGeneralSectionProps = {
     generalInfo?: GeneralInfoType;
 }
 
 const ReviewGeneralSection = ({ generalInfo }: ReviewGeneralSectionProps) => {
+    
     return (
         <div>
-            <Typography variant="h4" borderBottom={'1px solid black'} paddingLeft={'10px'} marginRight={'20px'} fontWeight={800}>General Information</Typography>
+            <Typography variant="h4" borderBottom={'1px solid black'} paddingLeft={'10px'} fontWeight={800}>General Information</Typography>
             {generalInfo && 
-                <div style={{ paddingLeft: '20px' }}>
-                    {generalInfo["first name"] && <Typography>{generalInfo["first name"]}</Typography>}
-                    {generalInfo["last name"] && <Typography>{generalInfo["last name"]}</Typography>}
-                    {generalInfo["email address"] && <Typography>{generalInfo["email address"]}</Typography>}
-                    {generalInfo["linkedin"] && <Typography>{generalInfo["linkedin"]}</Typography>}
-                    {generalInfo["phone number"] && <Typography>{generalInfo["phone number"]}</Typography>}
-                    {generalInfo["role_title"] && <Typography>{generalInfo["role_title"]}</Typography>}
-                    {generalInfo["languages"] && 
-                        <div>
+                <div style={{ paddingLeft: '20px', paddingTop: '15px', display: 'flex', flexDirection: 'column', gap: '12px' }}>
+                    {
+                        generalInfo?.["first name"] && 
+                            <div style={{ display: 'flex', alignItems: 'baseline'}}>
+                                <InputLabel sx={{ width: '120px' }}>First Name</InputLabel>
+                                <StyledTextField sx={{ flex: '1'}} variant="outlined" InputProps={{readOnly: true}} value={generalInfo["first name"]}/>
+                            </div>
+                    }
+                    {
+                        generalInfo?.["last name"] && 
+                            <div style={{ display: 'flex', alignItems: 'baseline'}}>
+                                <InputLabel sx={{ width: '120px' }}>Last Name</InputLabel>
+                                <StyledTextField sx={{ flex: '1'}} InputProps={{readOnly: true}} value={generalInfo["last name"]}/>
+                            </div>
+                    }
+                    {
+                        generalInfo?.["email address"] &&
+                            <div style={{ display: 'flex', alignItems: 'baseline'}}>
+                                <InputLabel sx={{ width: '120px' }}>Email Address</InputLabel>
+                                <StyledTextField sx={{ flex: '1'}} InputProps={{readOnly: true}} value={generalInfo["email address"]}/>
+                            </div>
+                    }
+                    {
+                        generalInfo?.["linkedin"] && 
+                            <div style={{ display: 'flex', alignItems: 'baseline'}}>
+                                <InputLabel sx={{ width: '120px' }}>LinkedIn</InputLabel>
+                                <StyledTextField sx={{ flex: '1'}} InputProps={{readOnly: true}} value={generalInfo["linkedin"]}/>
+                            </div>
+                    }   
+                    {
+                        generalInfo["phone number"] && 
+                        <div style={{ display: 'flex', alignItems: 'baseline'}}>
+                            <InputLabel sx={{ width: '120px' }}>Phone Number</InputLabel>
+                            <StyledTextField sx={{ flex: '1'}} InputProps={{readOnly: true}} value={generalInfo["phone number"]}/>
+                        </div>
+                    }
+                    {
+                        generalInfo["role_title"] && 
+                        <div style={{ display: 'flex', alignItems: 'baseline'}}>
+                            <InputLabel sx={{ width: '120px' }}>Role Title</InputLabel>
+                            <StyledTextField sx={{ flex: '1'}} InputProps={{readOnly: true}} value={generalInfo["role_title"]}/>
+                        </div>
+                    }
+                    {
+                        generalInfo["languages"] && 
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'baseline', gap: '12px'}}>
                             <Typography variant="h5">Languages</Typography>
                             {Object.keys(generalInfo["languages"]).map(item => {
                                 return (
-                                    <div style={{ display: 'flex' }}>
-                                        <Typography>-&nbsp;{LangList[item]}&nbsp;|&nbsp;</Typography>
-                                        <Typography>{LangLevel[generalInfo["languages"][item]]}</Typography>
+                                    <div style={{ display: 'flex', alignItems: 'baseline', width: '100%', paddingLeft: '12px'}}>
+                                        <InputLabel sx={{ width: '108px' }}>{LangList[item]}</InputLabel>
+                                        <StyledTextField sx={{ flex: '1' }} InputProps={{readOnly: true}} value={LangLevel[generalInfo["languages"][item]]} />
                                     </div>
                                 )
                             })}
