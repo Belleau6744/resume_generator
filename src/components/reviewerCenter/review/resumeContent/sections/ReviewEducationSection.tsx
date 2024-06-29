adaea319c2326a4d5d4b6359a9e9b5d39499cfb4
Jérémy Belleau <belleauj22@gmail.com>
2024-06-29 15:31:06 -0400

    update education layout

--- src/components/reviewerCenter/review/resumeContent/sections/ReviewEducationSection.tsx
+++ src/components/reviewerCenter/review/resumeContent/sections/ReviewEducationSection.tsx
@@ -1,5 +1,11 @@
-import { Typography } from "@mui/material";
+import { InputLabel, styled, TextField, Typography } from "@mui/material";
 import { EducationList } from "@types";
+
+const StyledTextField = styled(TextField)({
+    '& .MuiInputBase-input': {
+      caretColor: 'transparent', // Hides the caret
+    },
+  });
 
 type ReviewEducationSectionProps = {
     education?: EducationList;
@@ -7,17 +13,42 @@ type ReviewEducationSectionProps = {
 
 const ReviewEducationSection = ({ education }: ReviewEducationSectionProps) => {
     return (
-        <div>
-            <Typography variant="h4" borderBottom={'1px solid black'} paddingLeft={'10px'} marginRight={'20px'} fontWeight={800}>Education</Typography>
+        <div style={{ marginBottom: '20px' }}>
+            <Typography variant="h4" borderBottom={'1px solid black'} paddingLeft={'10px'} marginRight={'20px'} marginBottom={'20px'} fontWeight={800}>Education</Typography>
             {education && Object.keys(education).map(item => {
                 return (
-                    <div>
-                        <Typography>{education[item].degree}</Typography>
-                        <Typography>{education[item]["end date"]}</Typography>
-                        <Typography>{education[item]["field of study"]}</Typography>
-                        <Typography>{education[item]["school address"]}</Typography>
-                        <Typography>{education[item]["school name"]}</Typography>
-                        <Typography>{education[item]["start date"]}</Typography>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: '12px'}}>
+                        <Typography variant="h5">&#x2022;&nbsp;{education[item].degree}</Typography>
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '12px', alignItems: 'baseline', paddingLeft: '12px', width: '100%' }}>
+                            {
+                            education[item]["field of study"] && 
+                                <div style={{ display: 'flex', alignItems: 'baseline', width: '100%'}}>
+                                        <InputLabel sx={{ width: '120px' }}>Field Of Study</InputLabel>
+                                        <StyledTextField sx={{ flex: '1'}} variant="outlined" InputProps={{readOnly: true}} value={education[item]["field of study"]}/>
+                                </div>
+                            }
+                            {
+                            education[item]["school address"] && 
+                                <div style={{ display: 'flex', alignItems: 'baseline', width: '100%'}}>
+                                        <InputLabel sx={{ width: '120px' }}>School Address</InputLabel>
+                                        <StyledTextField sx={{ flex: '1'}} variant="outlined" InputProps={{readOnly: true}} value={education[item]["school address"]}/>
+                                </div>
+                            }
+                            {
+                            education[item]["school name"] && 
+                                <div style={{ display: 'flex', alignItems: 'baseline', width: '100%'}}>
+                                        <InputLabel sx={{ width: '120px' }}>School Name</InputLabel>
+                                        <StyledTextField sx={{ flex: '1'}} variant="outlined" InputProps={{readOnly: true}} value={education[item]["school name"]}/>
+                                </div>
+                            }
+                            {
+                            education[item]["end date"] && education[item]["start date"] &&
+                                <div style={{ display: 'flex', alignItems: 'baseline', width: '100%'}}>
+                                        <InputLabel sx={{ width: '120px' }}>Dates</InputLabel>
+                                        <StyledTextField sx={{ flex: '1'}} variant="outlined" InputProps={{readOnly: true}} value={`${education[item]["start date"]} - ${education[item]["end date"]}`}/>
+                                </div>
+                            }
+                        </div>
                     </div>
                 )
             })}
