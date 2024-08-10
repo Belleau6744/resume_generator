b52f5f6914030979768f37119c50f5b866940bd7
Jérémy Belleau <belleauj22@gmail.com>
2024-08-10 19:00:18 -0400

    additional languages in picker

--- src/utils/Languages.ts
+++ src/utils/Languages.ts
@@ -2,7 +2,25 @@ export const LangList = {
     'fra': 'Francais',
     'eng': 'English',
     'spa': 'Espanol',
-}
+    'ger': 'Deutsch',
+    'ita': 'Italiano',
+    'rus': 'Русский',
+    'por': 'Português',
+    'jpn': '日本語',
+    'kor': '한국어',
+    'chn': '中文',
+    'ara': 'العربية',
+    'hin': 'हिन्दी',
+    'tur': 'Türkçe',
+    'dut': 'Nederlands',
+    'gre': 'Ελληνικά',
+    'heb': 'עברית',
+    'vie': 'Tiếng Việt',
+    'pol': 'Polski',
+    'swe': 'Svenska',
+    'dan': 'Dansk'
+};
+
 
 export const LangLevel = {
     '0' : 'Basic',
